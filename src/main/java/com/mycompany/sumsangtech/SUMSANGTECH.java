d59c6e9183a8d53c72257d3fadff8dbd00787a8b
Earl John Pulido <89810628+sleep-deepfried@users.noreply.github.com>
2023-03-08 12:34:47 +0800

    Add files via upload

---
+++ src/main/java/com/mycompany/sumsangtech/SUMSANGTECH.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.sumsangtech;
+
+/**
+ *
+ * @author Earl
+ */
+public class SUMSANGTECH {
+
+    public static void main(String[] args) {
+      new loginFrame().setVisible(true);
+    }
+}
